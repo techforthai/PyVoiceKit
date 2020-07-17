25d53bbd5d0b7208d67b032ee5e9c4c70528efbd
Phoomparin Mano <phoomparin@gmail.com>
2020-07-18 01:53:28 +0700

    feat: add button to run code

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,6 +11,7 @@
         .editor-title Code Editor
       .editor-container
         codemirror.editor-area(v-model="code" :options="cmOptions")
+      button.btn-run-code(@click="runCode") Run Code
 </template>
 
 <style lang="sass" scoped>
@@ -38,6 +39,7 @@
   min-height: 80vh
 
 .editor-panel
+  position: relative
   background: #282a36
   box-shadow: 1px 1px 8px 4px rgba(0, 0, 0, 0.3)
   z-index: 1
@@ -88,12 +90,33 @@
 
 .dot.green
   background: #53c32b
+
+button.btn-run-code
+  font-size: 24px
+  font-family: "JetBrains Mono", monospace
+  color: white
+  background: linear-gradient(-225deg, rgb(241, 95, 121) 35%, rgb(178, 69, 146) 100%)
+  cursor: pointer
+  border: none
+  padding: 8px 15px
+  position: absolute
+  bottom: 18px
+  right: 20px
+  box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.5)
+  border-radius: 8px
+  z-index: 10
+  transition: 1s all cubic-bezier(0.19, 0.16, 0, 1.27)
+
+  &:hover
+    transform: scale(1.15)
 </style>
 
 <script lang="ts">
 import Vue from 'vue'
 import { codemirror } from 'vue-codemirror'
 import { EditorConfiguration } from 'codemirror'
+
+import { app } from '../main'
 
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/python/python.js'
@@ -110,17 +133,22 @@ const cmOptions: EditorConfiguration = {
   viewportMargin: Infinity,
 }
 
-const starterCode = `
-def on_voice(text):
-  if "sleep" in text:
-    youtube("Sleeping and Meditation Music")
-    say("Sleep tight!")
-    return
+const starterCode = `print("Hello, World!")`.trim()
 
-  if "monday" in text:
-    show_color("yellow")
-    show_text("Good Morning! 🍹")
-`.trim()
+function run(code: string) {
+  if (!window.__BRYTHON__) return
+
+  const js = window.__BRYTHON__.python_to_js(code)
+
+  console.groupCollapsed('ℹ️ Translating Python to JavaScript')
+  console.log('Python:', code)
+  console.log('JavaScript:', js)
+  console.groupEnd()
+
+  return eval.call(this, js)
+}
+
+window.run = run
 
 export default Vue.extend({
   components: { codemirror },
@@ -128,6 +156,28 @@ export default Vue.extend({
   data: () => ({
     code: starterCode,
     cmOptions,
+    logs: [],
   }),
+
+  mounted() {
+    window.app = this
+  },
+
+  methods: {
+    patch() {
+      window.__BRYTHON__.builtins.print = (...args) => {
+        console.log(`[Python] ${args.join('\n')}`)
+        this.logs = [...this.logs, args.join('\n')]
+      }
+    },
+    runCode() {
+      try {
+        this.patch()
+        run(this.code)
+      } catch (error) {
+        console.warn('User Code Error!', error)
+      }
+    },
+  },
 })
 </script>
